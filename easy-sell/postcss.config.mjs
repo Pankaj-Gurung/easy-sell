744a872f6a2fd0311595ed27ee2d1b90a91a4588
Pankaj Gurung <grgpankaj004@gmail.com>
2025-03-10 11:04:25 +0545

    update: configure tailwind css and add footer & header components

--- easy-sell/postcss.config.mjs
+++ easy-sell/postcss.config.mjs
@@ -1,5 +1,20 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          951: '#f4eee5',
+          952: '#222',
+          953: '#E9E4DB',
+        },
+        green: {
+          951: '#9CAE96',
+        },
+      },
+    },
+  },
+  plugins: [],
 };
 
 export default config;
